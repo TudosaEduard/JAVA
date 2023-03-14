f9a567b22c1fdb725fc5264cc02259d4e79d2a05
Tudosa Eduard <tudosa.bogdan88@gmail.com>
2023-03-15 00:50:57 +0200

    start compulsory laboratory 4

---
+++ laborator4/src/main/java/compulsory/Compulsory.java
@@ -0,0 +1,38 @@
+package compulsory;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+import compulsory.objects.Project;
+import compulsory.objects.Student;
+
+public class Compulsory {
+    public void main()
+    {
+        var students = IntStream.rangeClosed(0, 3)
+                        .mapToObj(i -> new Student("S" + i) )
+                        .toArray(Student[]::new);
+
+        var projects = IntStream.rangeClosed(0, 2)
+                        .mapToObj(i -> new Project("P" + i) )
+                        .toArray(Project[]::new);
+
+        List<Student> listOfStudents = new LinkedList<>();
+        Set<Project> listOfProjects = new TreeSet<>();
+
+        for (Student s : students) {
+            listOfStudents.add(s);
+        }
+        Collections.sort(listOfStudents,((u, v) -> u.getName().compareTo(v.getName())));
+
+        for (Project p : projects) {
+            listOfProjects.add(p);
+        }
+
+        System.out.println(listOfStudents);
+        System.out.println(listOfProjects);
+    }
+}
---
+++ laborator4/src/main/java/compulsory/objects/Project.java
@@ -0,0 +1,19 @@
+package compulsory.objects;
+
+public class Project {
+    private String name;
+
+    public Project(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
+}
